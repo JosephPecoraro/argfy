d36f949c482f4c5227e808a10f68b1765ae5994a
Joseph Pecoraro <joepeck02@gmail.com>
2008-11-29 01:48:29 -0500

    Added sample.rb and README information about it

---
+++ sample.rb
@@ -0,0 +1,17 @@
+require 'ARGFy'
+
+argf = ARGFy.new(ARGV)
+argf.each do |line|
+
+	# Per File Header
+	if argf.new_file?
+		filename = argf.filename
+		filename = "STDIN" if filename == "-"
+		puts '', filename, "-"*filename.length
+	end
+
+	# Print out the line with line numbers
+	puts "%3d: %s" % [argf.lineno, line]
+
+end
+puts
